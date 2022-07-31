875ea005763100e2c949a089a20df36a0677c146
Gary Ditsch <garyditsch@gmail.com>
2022-07-31 11:27:07 -0400

    updated hero and made header conditional

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import { useRouter } from 'next/router'
 
 
 export default function Navbar(){
-    
+    const router = useRouter() 
 
-    return <nav className={styles.nav}>
+    return <nav 
+        className={styles.nav}  
+        style={{    
+            backgroundColor: router.pathname === '/' ? '#F7FAE6' : '#757013',
+            color: router.pathname === '/' ? 'black' : 'white'
+        }}
+    >
         <Link href = "/" className="siteLogo"><a><Image src={"/fitnessPlusCircleLogo-web.png"} alt="Fitness Plus Logo" width={75} height={75}/></a></Link>
         <ul>
             <li>
