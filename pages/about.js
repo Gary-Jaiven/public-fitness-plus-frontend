e42698f7c5907831b546192bff39f5d640440a4b
Gary Ditsch <garyditsch@gmail.com>
2022-08-14 18:07:39 -0400

    finished about page

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
     <>
       <div className='section_container_one_column'>
           <section className='section_content_one_column'>
-            <h1 style={{textAlign: 'center'}}>
+            <h1 className='title' style={{textAlign: 'center', color: '#757015', fontWeight: 'normal'}}>
             Lorem ipsum dolor sit amet
             </h1>
           </section>
@@ -46,14 +46,18 @@ export default function About() {
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et</p>
         </section>
       </div>
-        <section >
-            <h2>Get One-of-a-Kind Care</h2>
-            <h4>Fitness Plus is the longest-standing facility of its Kind in Central Kentucky</h4>
+      <div className='section_container_one_column' style={{backgroundColor: '#797A71', color: 'white'}}>
+        <section className='section_content_one_column'>
+            <div className='sub_title_white' style={{textAlign: 'center', marginBottom: '1rem'}}>Get One-of-a-Kind Care</div>
+            <div style={{textAlign: 'center', fontSize: '1.5rem'}}>Fitness Plus is the longest-standing facility of its Kind in Central Kentucky</div>
         </section>
-      <section>
-          <h2>Lorem ipsum dolor sit amet</h2>
-          <button>Schedule Your Free Consultation.</button>
-      </section>
+      </div>
+      <div className='section_container_one_column' style={{margin: '4rem 0rem'}}>
+        <section className='section_content_one_column'>
+          <div className='sub_title_black' style={{textAlign: 'center', marginBottom: '1rem'}}>Lorem ipsum dolor sit amet</div>
+          <button className='button' style={{margin: '0 auto'}}>Schedule Your Free Consultation.</button>
+        </section>
+      </div>
     </>
   )
 }
